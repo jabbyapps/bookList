48b64b7fa778a1c6552f4851117a7a87cfa62965
jabbyapps <gbarnett@jabbyapps.com>
2018-05-15 02:29:42 -0400

    app.js bookList update

--- app.js
+++ app.js
@@ -43,6 +43,13 @@ UI.prototype.showAlert = function(message, className) {
   }, 3000);
 };
 
+// Delete book
+UI.prototype.deleteBook = function(target) {
+  if (target.className === "delete") {
+    target.parentElement.parentElement.remove();
+  }
+};
+
 // Clear fields
 UI.prototype.clearFields = function() {
   document.getElementById("title").value = "";
@@ -50,7 +57,7 @@ UI.prototype.clearFields = function() {
   document.getElementById("isbn").value = "";
 };
 
-// Event Listeners
+// Event Listener for add book
 document.getElementById("book-form").addEventListener("submit", function(e) {
   // Get form values
   const title = document.getElementById("title").value,
@@ -76,6 +83,19 @@ document.getElementById("book-form").addEventListener("submit", function(e) {
     // Clear fields
     ui.clearFields();
   }
+
+  e.preventDefault();
+});
+
+// Event Listener for delete
+document.getElementById("book-list").addEventListener("click", function(e) {
+  // Instantiate UI
+  const ui = new UI();
+  // UI delete book
+  ui.deleteBook(e.target);
+
+  // Show message
+  ui.showAlert("Book removed!", "success");
 
   e.preventDefault();
 });
